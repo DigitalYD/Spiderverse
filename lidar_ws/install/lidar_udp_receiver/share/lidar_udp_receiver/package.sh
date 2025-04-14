a6a8c6b2a8f6eb9e4c153012510eed3e161884a6
Asher-Goods <asheragoodwin@gmail.com>
2025-04-14 19:43:06 -0400

    reorganizing files

--- lidar_ws/install/lidar_udp_receiver/share/lidar_udp_receiver/package.sh
+++ lidar_ws/install/lidar_udp_receiver/share/lidar_udp_receiver/package.sh
@@ -80,6 +80,7 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/lidar_udp_receiver/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/lidar_udp_receiver/hook/ament_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/lidar_udp_receiver/share/lidar_udp_receiver/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
